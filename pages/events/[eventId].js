996006fd7cceae26102130b13ad577c0d0989b06
guntz <lambo1234@hotmail.com>
2023-03-31 22:48:04 +0700

    finish1

---
+++ pages/events/[eventId].js
@@ -0,0 +1,37 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+import { getEventById } from "@/dummy-data";
+import EventSummary from "@/components/event-detail/event-summary";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventContent from "@/components/event-detail/event-content";
+import ErrorAlert from "@/components/ui/error-alert";
+
+function EventDetailPage() {
+  const router = useRouter();
+  const eventID = router.query.eventId;
+  const event = getEventById(eventID);
+
+  if (!event) {
+    return (
+      <ErrorAlert>
+        <p>No event Found!</p>
+      </ErrorAlert>
+    );
+  }
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
+}
+
+export default EventDetailPage;
