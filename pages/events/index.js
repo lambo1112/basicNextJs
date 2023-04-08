2095372224ad8d8ecf0fc23a6e89bec9118ddb46
guntz <lambo1234@hotmail.com>
2023-04-08 23:30:27 +0700

    complete2.1

--- pages/events/index.js
+++ pages/events/index.js
@@ -4,6 +4,7 @@ import { useRouter } from "next/router";
 import { getAllEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
+import Head from "next/head";
 
 function AllEventsPage(props) {
   const router = useRouter();
@@ -17,6 +18,13 @@ function AllEventsPage(props) {
 
   return (
     <Fragment>
+      <Head>
+        <title>All Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+      </Head>
       <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
