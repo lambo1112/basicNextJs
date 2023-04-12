de20f4e67ed66ccc3237f09b096912214cff5025
guntz <lambo1234@hotmail.com>
2023-04-13 00:06:37 +0700

    comment1.3.1

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,16 +1,22 @@
-import fs from "fs";
-import path from "path";
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
     if (!userEmail || !userEmail.includes("@")) {
       res.status(422).json({ message: "Invalid email Address." });
       return;
     }
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://maximilian:aMMFuswMRKAVWG5T@atlascluster.ustohbd.mongodb.net/events?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    await db.collection("newsletter").insertOne({ email: userEmail });
+    client.close();
+
     res.status(201).json({ message: "Signed up!" });
   }
-  const filepath = path.join(process.cwd(), "data", "lettler.json");
-  const fileData = fs.readFileSync(filepath);
 }
 
 export default handler;
