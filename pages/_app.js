e6bd5ecb11e887e74603331f6bf49bb636c237dc
guntzdevCom <lambo1234@hotmail.com>
2023-04-13 16:08:49 +0700

    complete2.1

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
+import Notification from "@/components/ui/notification";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
+      <Notification title="Test" message="This is a test." status="pending" />
     </Layout>
   );
 }
---
+++ store/notification-context.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+createContext({
+  notification: null, //{title,message,status}
+});
