1c1dc73376eb7c4990845af48a1c3dcda46ebfdb
guntz <lambo1234@hotmail.com>
2023-04-14 22:34:32 +0700

    complete4

--- store/notification-context.js
+++ store/notification-context.js
@@ -1,5 +1,47 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
-createContext({
+const NotificationContext = createContext({
   notification: null, //{title,message,status}
+  showNotification: function (notificationData) {},
+  hideNotification: function () {},
 });
+
+export function NotificationContextProvider(props) {
+  const [activeNotification, setActiveNotification] = useState();
+  useEffect(() => {
+    if (
+      activeNotification &&
+      (activeNotification.status === "success" ||
+        activeNotification.status === "error")
+    ) {
+      const timer = setTimeout(() => {
+        hideNotificationHandler();
+      }, 2000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
+  return (
+    <NotificationContext.Provider value={context}>
+      {props.children}
+    </NotificationContext.Provider>
+  );
+}
+
+export default NotificationContext;
