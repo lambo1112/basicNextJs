8d0b7522857282d427a2d55e982fcd438b74bd3e
guntz <lambo1234@hotmail.com>
2023-04-16 23:38:53 +0700

    completeFullBlog

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,30 +1,89 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import classes from "./contact-form.module.css";
+import Notification from "../ui/notification";
 
 function ContactForm() {
   const refEmail = useRef();
   const refName = useRef();
   const refMessage = useRef();
-  function sendMessageHandler(event) {
+  const [requestStatus, setRequestStatus] = useState(); // pending success error
+  const [requestError, setRequestError] = useState();
+
+  useEffect(() => {
+    if (requestStatus === "success" || requestStatus === "error") {
+      const timer = setTimeout(() => {
+        setRequestStatus(null);
+        setRequestError(null);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [requestStatus]);
+
+  async function sendContectData(contactDetails) {
+    //optional: add client-side validation
+    const response = await fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify(contactDetails),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message || "Something went wrong!");
+    }
+  }
+
+  async function sendMessageHandler(event) {
     event.preventDefault();
     const enteredEmail = refEmail.current.value;
     const enteredName = refName.current.value;
     const enteredMessage = refMessage.current.value;
 
-    //optional: add client-side validation
-    fetch("api/contact", {
-      method: "POST",
-      body: JSON.stringify({
+    try {
+      setRequestStatus("pending");
+      await sendContectData({
         email: enteredEmail,
         name: enteredName,
         message: enteredMessage,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+      });
+      setRequestStatus("success");
+      refEmail.current.value = "";
+      refName.current.value = "";
+      refMessage.current.value = "";
+    } catch (error) {
+      setRequestError(error.message);
+      setRequestStatus("error");
+    }
   }
 
+  let notification;
+
+  if (requestStatus === "pending") {
+    notification = {
+      status: "pending",
+      title: "Sending message...",
+      message: "Your message is on its way!",
+    };
+  }
+
+  if (requestStatus === "success") {
+    notification = {
+      status: "success",
+      title: "Success!",
+      message: "Message sent successfully",
+    };
+  }
+  if (requestStatus === "error") {
+    notification = {
+      status: "error",
+      title: "Error!",
+      message: requestError,
+    };
+  }
   return (
     <section className={classes.contact}>
       <h1> How can I help you?</h1>
@@ -47,6 +106,7 @@ function ContactForm() {
           <button>Send Message</button>
         </div>
       </form>
+      {notification && <Notification {...notification} />}
     </section>
   );
 }
