ce14e4a8491ad40ef7dc7faa54631ca97daa5647
guntz <lambo1234@hotmail.com>
2023-04-19 23:07:24 +0700

    prepared for deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+const nextConfig = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      reactStrictMode: true,
+      env: {
+        mongodb_username: "maximilian",
+        mongodb_password: "aMMFuswMRKAVWG5T",
+        mongodb_clustername: "atlascluster",
+        mongodb_database: "my-site-dev",
+      },
+    };
+  }
+  return {
+    reactStrictMode: true,
+    env: {
+      mongodb_username: "maximilian",
+      mongodb_password: "aMMFuswMRKAVWG5T",
+      mongodb_clustername: "atlascluster",
+      mongodb_database: "my-site",
+    },
+  };
+};
+
+module.exports = nextConfig;
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -23,10 +23,10 @@ async function handler(req, res) {
       message,
     };
     let client;
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.ustohbd.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
+
     try {
-      client = await MongoClient.connect(
-        "mongodb+srv://maximilian:aMMFuswMRKAVWG5T@atlascluster.ustohbd.mongodb.net/my-site?retryWrites=true&w=majority"
-      );
+      client = await MongoClient.connect(connectionString);
     } catch (error) {
       res.status(500).json({ message: "Could not connect to database." });
       return;
