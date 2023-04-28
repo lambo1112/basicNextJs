73d96c696703742db632b66666677cae5b4f593f
guntz <lambo1234@hotmail.com>
2023-04-29 02:13:30 +0700

    authSSR

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,33 @@
+import { verifyPassword } from "@/lib/auth";
+import { connectToDatabase } from "@/lib/db";
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+export default NextAuth({
+  session: { jwt: true },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        const client = await connectToDatabase();
+        const usersCollection = client.db().collection("users");
+        const user = await usersCollection.findOne({
+          email: credentials.email,
+        });
+        if (!user) {
+          client.close();
+          throw new Error("No user found!");
+        }
+        const isValid = await verifyPassword(
+          credentials.password,
+          user.password
+        );
+
+        if (!isValid) {
+          client.close();
+          throw new Error("Could not login!");
+        }
+        client.close();
+        return { email: user.email };
+      },
+    }),
+  ],
+});
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,20 +2,21 @@ import { hashPassword } from "@/lib/auth";
 import { connectToDatabase } from "@/lib/db";
 
 async function handler(req, res) {
+  if (req.method !== "POST") {
+    return;
+  }
   const data = req.body;
   const { email, password } = data;
-
   if (
     !email ||
-    !email.include("@") ||
+    !email.includes("@") ||
     !password ||
     password.trim().length < 7
   ) {
-    res.status(422),
-      json({
-        message:
-          "Invalid input - password should also be at least 7 characters long.",
-      });
+    res.status(422).json({
+      message:
+        "Invalid input - password should also be at least 7 characters long.",
+    });
     return;
   }
 
@@ -23,7 +24,15 @@ async function handler(req, res) {
 
   const db = client.db();
 
-  const hashedPassword = hashPassword(password);
+  const existingUser = await db.collection("users").findOne({ email: email });
+
+  if (existingUser) {
+    res.status(422).json({ message: "User is already!" });
+    client.close();
+    return;
+  }
+
+  const hashedPassword = await hashPassword(password);
 
   const result = await db.collection("users").insertOne({
     email: email,
@@ -31,6 +40,7 @@ async function handler(req, res) {
   });
 
   res.status(201).json({ message: "Created user!!" });
+  client.close();
 }
 
 export default handler;
